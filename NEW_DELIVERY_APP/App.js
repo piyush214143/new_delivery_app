ca0ffb2eeab803c7c1ca5f055c541719aa12d854
vaishu-15 <vaishali15yadav@gmail.com>
2024-03-14 11:17:59 +0530

    initialized and setup

--- NEW_DELIVERY_APP/App.js
+++ NEW_DELIVERY_APP/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { View,Text } from "react-native";
 
 const App =() =>{
 return (
-  
+  <View>
+    <Text style={{justifyContent:'center',alignSelf:'center',color:'black',paddingTop:150}}>React native
+    </Text>
+  </View>
 )
 }
 
