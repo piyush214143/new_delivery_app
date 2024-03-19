2aa982e2b94b84ba59c717183ef42765c9d2bf64
vaishu-15 <vaishali15yadav@gmail.com>
2024-03-19 18:31:25 +0530

    main screen navigation

--- NEW_DELIVERY_APP/App.js
+++ NEW_DELIVERY_APP/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import { StatusBar, View } from 'react-native';
 import Routes from './src/navigation/Routes';
 
-
 const App = (props) => {
-  const [switchToTabs, setSwitchToTabs] = useState(true);
+  const [switchToTabs, setSwitchToTabs] = useState(false);
+
+  const switchToTabsHandler = () => {
+    setSwitchToTabs(true);
+  };
 
   return (
    
     <View style={{ flex: 1 }}>
       <StatusBar backgroundColor={'black'} translucent />
-      <Routes switchToTabs={switchToTabs} /> 
+      
+      <Routes switchToTabs={switchToTabs} />
     </View>
     
   );
