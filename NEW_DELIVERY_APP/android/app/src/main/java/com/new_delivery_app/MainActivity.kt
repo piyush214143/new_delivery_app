0d21434b9b12f8a23b3c3d8b5f446eadc7be93ad
vaishu-15 <vaishali15yadav@gmail.com>
2024-04-01 10:42:15 +0530

    splash screen added for android

--- NEW_DELIVERY_APP/android/app/src/main/java/com/new_delivery_app/MainActivity.kt
+++ NEW_DELIVERY_APP/android/app/src/main/java/com/new_delivery_app/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.new_delivery_app
 
+import android.os.Bundle
+import org.devio.rn.splashscreen.SplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +9,21 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "NEW_DELIVERY_APP"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "NEW_DELIVERY_APP"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this)
+        super.onCreate(savedInstanceState)
+    }
 }
