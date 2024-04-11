cafac3766063be53a6694ad4752ab451a311e7db
vaishu-15 <vaishali15yadav@gmail.com>
2024-04-11 11:10:18 +0530

    components created in payment

--- NEW_DELIVERY_APP/src/screen/Payment/index.js
+++ NEW_DELIVERY_APP/src/screen/Payment/index.js
@@ -2,7 +2,6 @@ import React, {useState, useRef, useEffect} from 'react';
 import {
   View,
   Text,
-  TextInput,
   Animated,
   TouchableOpacity,
   Easing,
@@ -10,14 +9,11 @@ import {
   ScrollView,
   Platform,
   PermissionsAndroid,
-  PermissionsIOS,
-  ImageBackground,
-  Dimensions,
 } from 'react-native';
 import paymentStyle from './style';
 import categoryStyle from '../Categories/style';
-import {COLORS, IMAGES} from '../../utils/constants';
-import {CardScanView} from '@cardscan.ai/insurance-cardscan-react-native';
+import {IMAGES} from '../../utils/constants';
+import {CardDisplay, CardInput} from '../../common/creditDetails';
 
 const Payment = props => {
   const [cardNumber, setCardNumber] = useState('');
@@ -30,27 +26,6 @@ const Payment = props => {
   const [isScannerOpen, setIsScannerOpen] = useState(false);
   const [cameraPermissionGranted, setCameraPermissionGranted] = useState(false);
   const [containsSpecialChars, setContainsSpecialChars] = useState(false);
-  // const [orientation, setOrientation] = useState(
-  //   Dimensions.get('window').width > Dimensions.get('window').height
-  //     ? 'landscape'
-  //     : 'portrait',
-  // );
-
-  // useEffect(() => {
-  //   const updateOrientation = () => {
-  //     setOrientation(
-  //       Dimensions.get('window').width > Dimensions.get('window').height
-  //         ? 'landscape'
-  //         : 'portrait',
-  //     );
-  //   };
-
-  //   Dimensions.addEventListener('change', updateOrientation);
-
-  //   return () => {
-  //     Dimensions.removeEventListener('change', updateOrientation);
-  //   };
-  // }, []);
 
   useEffect(() => {
     requestCameraPermission();
@@ -75,16 +50,6 @@ const Payment = props => {
           setCameraPermissionGranted(false);
         }
       }
-      // else if (Platform.OS === 'ios') {
-      //   const granted = await PermissionsIOS.request(
-      //     PermissionsIOS?.PERMISSIONS?.CAMERA
-      //   );
-      //   if (granted === 'granted') {
-      //     setCameraPermissionGranted(true);
-      //   } else {
-      //     setCameraPermissionGranted(false);
-      //   }
-      // }
     } catch (err) {
       console.warn(err);
       setCameraPermissionGranted(false);
@@ -123,7 +88,7 @@ const Payment = props => {
   };
 
   const formatCardName = name => {
-   const cleanedName = name.replace(/"[A-Za-z]"/g, '').trim();
+    const cleanedName = name.replace(/"[A-Za-z]"/g, '').trim();
     const formattedName = cleanedName
       .toLowerCase()
       .split(' ')
@@ -165,40 +130,12 @@ const Payment = props => {
       return;
     }
   };
-  const handleFlip = () => {
-    setIsFlipped(!isFlipped);
-    Animated.timing(flipAnimation, {
-      toValue: isFlipped ? 0 : 1,
-      duration: 1000,
-      easing: Easing.linear,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const frontInterpolate = flipAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '180deg'],
-  });
-
-  const backInterpolate = flipAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['180deg', '360deg'],
-  });
-
-  const frontAnimatedStyle = {
-    transform: [{rotateY: frontInterpolate}],
-  };
-
-  const backAnimatedStyle = {
-    transform: [{rotateY: backInterpolate}],
-  };
 
   return (
     <ScrollView
       contentContainerStyle={{flexGrow: 1}}
       showsVerticalScrollIndicator={false}>
       <View style={paymentStyle.container}>
-        {/* <View style={[paymentStyle.container,{padding: orientation === 'landscape' ? 40 : 20,}]}>*/}
         <View style={categoryStyle.header}>
           <TouchableOpacity onPress={() => props.navigation.goBack('')}>
             <Image source={IMAGES.arrow} style={categoryStyle.arrow} />
@@ -206,175 +143,37 @@ const Payment = props => {
           <Text style={categoryStyle.heading}>Credit / Debit card</Text>
         </View>
         <View style={{flex: 1}}>
-          <View style={paymentStyle.cardContainer}>
-            {isScannerOpen ? (
-              <CardScanView
-                style={paymentStyle.card}
-                // sessionToken={null}
-                onSuccess={card => {
-                  setCardNumber(card.number);
-                  setExpiry(card.expiry);
-                  setCVV(card.cvv);
-                  setIsScannerOpen(false);
-                }}
-                onError={error => {
-                  console.error('Error scanning card:', error);
-                  setIsScannerOpen(false);
-                }}
-              />
-            ) : (
-              <TouchableOpacity onPress={handleFlip}>
-                <View
-                  style={[
-                    paymentStyle.card,
-                    isFlipped ? paymentStyle.flippedCard : null,
-                  ]}>
-                  <Animated.View
-                    style={[paymentStyle.cardFace, frontAnimatedStyle]}>
-                    <ImageBackground
-                      source={IMAGES.cardBase}
-                      resizeMode="cover"
-                      style={paymentStyle.base}>
-                      <Image
-                        source={IMAGES.basePart}
-                        style={paymentStyle.basePart}
-                      />
-                      <Image
-                        source={IMAGES.mcSymbol}
-                        style={paymentStyle.mcSymbol}
-                      />
-                      <Text style={paymentStyle.cardValue}>
-                        {cardNumber || '4747 4747 4747 4747'}
-                      </Text>
-                      <View style={paymentStyle.cardRow}>
-                        <Text style={paymentStyle.cardHolder}>
-                          {cardName || 'VAISHALI YADAV'}
-                        </Text>
-                        <Text style={paymentStyle.cardExpiry}>
-                          {expiry || '02/26'}
-                        </Text>
-                      </View>
-                    </ImageBackground>
-                    {/* </ImageBackground> */}
-                  </Animated.View>
-                  <Animated.View
-                    style={[
-                      paymentStyle.cardFace,
-                      paymentStyle.cardFaceBack,
-                      backAnimatedStyle,
-                    ]}>
-                    <ImageBackground
-                      source={IMAGES.cardBase}
-                      resizeMode="cover"
-                      style={paymentStyle.base}>
-                      <Image
-                        source={IMAGES.basePart}
-                        style={paymentStyle.basePart}
-                      />
-                      <Text style={paymentStyle.cardValueFlipped}>
-                        {cvv || '***'}
-                      </Text>
-                    </ImageBackground>
-                  </Animated.View>
-                </View>
-              </TouchableOpacity>
-            )}
-          </View>
-          <View style={paymentStyle.inputContainer}>
-            <TouchableOpacity onPress={handleOpenCardScanner}>
-              <Image source={IMAGES.photoIcon} style={paymentStyle.photoIcon} />
-            </TouchableOpacity>
-            <View>
-              <Text style={paymentStyle.cardLabel}>Name on card</Text>
-              <TextInput
-                style={paymentStyle.input}
-                placeholder="Vaishali Yadav"
-                placeholderTextColor={COLORS.dGrey}
-                onChangeText={handleCardNameChange}
-                maxLength={20}
-              />
-              {containsSpecialChars && (
-                <Text style={paymentStyle.invalid}>
-                  Special characters and numbers are not allowed
-                </Text>
-              )}
-              {submitted && !validateCardName(cardName) && (
-                <Text style={paymentStyle.invalid}>
-                  This is a required field{' '}
-                </Text>
-              )}
-            </View>
-            <View>
-              <Text style={paymentStyle.cardLabel}>Card number</Text>
-              <View style={paymentStyle.cardNumberRow}>
-                <TextInput
-                  style={paymentStyle.input}
-                  placeholderTextColor={COLORS.dGrey}
-                  onChangeText={handleCardNumberChange}
-                  value={cardNumber}
-                  maxLength={19}
-                  keyboardType="numeric"
-                />
-                {cardNumber.length === 0 && (
-                  <Text style={paymentStyle.placeholder}>
-                    **** **** **** ****
-                  </Text>
-                )}
-                <Image
-                  source={IMAGES.cardSymbol}
-                  style={paymentStyle.cardSymbol}
-                />
-              </View>
-              {submitted && !validateCardNumber(cardNumber) && (
-                <Text style={paymentStyle.invalid}>
-                  This is a required field
-                </Text>
-              )}
-            </View>
-            <View style={paymentStyle.detailCard}>
-              <View>
-                <Text style={paymentStyle.cardLabel}>Expiry date</Text>
-                <TextInput
-                  style={paymentStyle.inputRow}
-                  placeholder="02/26"
-                  placeholderTextColor={COLORS.dGrey}
-                  onChangeText={handleExpiryChange}
-                  maxLength={4}
-                  keyboardType="numeric"
-                />
-                {submitted && !validateExpiry(expiry) && (
-                  <Text style={paymentStyle.invalid}>
-                    This is a required field
-                  </Text>
-                )}
-              </View>
-              <View>
-                <Text style={paymentStyle.cardLabel}>CVV</Text>
-                <View style={paymentStyle.cardNumberRow}>
-                  <TextInput
-                    style={paymentStyle.inputRow}
-                    placeholder="***"
-                    placeholderTextColor={COLORS.dGrey}
-                    onChangeText={handleCVVChange}
-                    maxLength={3}
-                    secureTextEntry={true}
-                    keyboardType="numeric"
-                  />
-                  <Image source={IMAGES.cvv} style={paymentStyle.cardSymbol} />
-                </View>
-                {submitted && !validateCVV(cvv) && (
-                  <Text style={paymentStyle.invalid}>
-                    This is a required field
-                  </Text>
-                )}
-              </View>
-            </View>
-            <TouchableOpacity
-              style={paymentStyle.btn1Container}
-              onPress={handleSubmit}>
-              <Text style={paymentStyle.btn1}>USE THIS CARD</Text>
-            </TouchableOpacity>
-          </View>
+          {isScannerOpen ? (
+            <CardScanner onSuccess={onSuccess} onError={onError} />
+          ) : (
+            <CardDisplay
+              cardNumber={cardNumber}
+              cardName={cardName}
+              expiry={expiry}
+              cvv={cvv}
+              isFlipped={isFlipped}
+              flipAnimation={flipAnimation}
+              setIsFlipped={setIsFlipped}
+            />
+          )}
+          <CardInput
+            handleCardNameChange={handleCardNameChange}
+            handleCardNumberChange={handleCardNumberChange}
+            handleExpiryChange={handleExpiryChange}
+            handleCVVChange={handleCVVChange}
+            cardNumber={cardNumber}
+            cardName={cardName}
+            expiry={expiry}
+            cvv={cvv}
+            submitted={submitted}
+            containsSpecialChars={containsSpecialChars}
+            validateCardName={validateCardName}
+            validateCardNumber={validateCardNumber}
+            validateExpiry={validateExpiry}
+            validateCVV={validateCVV}
+            handleSubmit={handleSubmit}
+            handleOpenCardScanner={handleOpenCardScanner}
+          />
         </View>
       </View>
     </ScrollView>
